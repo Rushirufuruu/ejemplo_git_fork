41e08198a493e04d73a08f18230457604676558c
Rushirufuruu <vicente.zapatasilva@gmail.com>
2024-05-02 15:02:19 -0400

    colo colo campeon

---
+++ zapata.py
@@ -0,0 +1,29 @@
+'''Cantemos todos de arica a Magallanes
+Por Colo-Colo ejemplo de valor
+Por su hidalguía no hay nadie quien lo gane
+Porque defiende con gloria el tricolor
+
+Es Colo-Colo como el gran araucano
+Que va a la lucha tenaz sin descansar
+Porque el recuerdo de David Arellano
+Siempre lo guía por la senda triunfal
+
+Colo-Colo, Colo-Colo, el equipo que ha sabido ser campeón
+Y en las lides deportivas pone siempre su chileno corazón
+Colo-Colo, Colo-Colo, representa nuestra raza sin igual
+Por su empuje y coraje en las canchas como el Colo-Colo no hay
+
+Es Colo-Colo valiente, fuerte y grande
+De sangre altiva y noble corazón
+Su nombre vibra desde el mar a los Andes
+Y hay en sus triunfos esfuerzo y emoción
+
+Laureles deja por todos los caminos
+Que virilmente luchando conquistó
+Antorcha inmensa de gloria es su destino
+Que alumbra siempre con fuego de campeón
+
+Colo-Colo, Colo-Colo, el equipo que ha sabido ser campeón
+Y en las lides deportivas pone siempre su chileno corazón
+Colo-Colo, Colo-Colo, representa nuestra raza sin igual
+Por su empuje y coraje en las canchas como Colo-Colo no hay'''
